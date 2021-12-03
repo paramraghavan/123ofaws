05e2cfb82416342903c2516df542441847b35012
Param Raghavan <paramraghavan@gmail.com>
2021-12-03 04:24:22 -0500

    sample cdk

---
+++ cloudformation/cdk/demo_app/tests/unit/test_demo_app_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from demo_app.demo_app_stack import DemoAppStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in demo_app/demo_app_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = DemoAppStack(app, "demo-app")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
